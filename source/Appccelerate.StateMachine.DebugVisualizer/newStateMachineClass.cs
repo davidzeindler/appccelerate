1fa7255c8fd669a113c76eff86df8794ae083054
David Zeindler <david.zeindler@bbv.ch>
2012-03-06 10:45:47 +0100

    StateMachine: Test Github by creating new class

---
+++ source/Appccelerate.StateMachine.DebugVisualizer/newStateMachineClass.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="newStateMachineClass.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Appccelerate.StateMachine.DebugVisualizer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class NewStateMachineClass
+    {
+        private string classname = "newStateMachineClass";
+    }
+}
